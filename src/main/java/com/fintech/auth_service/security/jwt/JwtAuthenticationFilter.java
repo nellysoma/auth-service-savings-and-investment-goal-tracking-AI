34d2a569a7d91b85c975ce6e466d926421cc195d
Harmony <nellysoma2005@yahoo.com>
2026-01-19 14:33:04 +0100

    Added the JwtAuthenticationFilter Class

--- src/main/java/com/fintech/auth_service/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/fintech/auth_service/security/jwt/JwtAuthenticationFilter.java
@@ -18,11 +18,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
     }
 
     @Override
-    protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
 
