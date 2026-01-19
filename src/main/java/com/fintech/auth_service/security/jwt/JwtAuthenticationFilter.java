b7d69d79f39f5ed9f7598595b047478d3e4f613c
Harmony <nellysoma2005@yahoo.com>
2026-01-19 14:09:49 +0100

    Added the JwtAuthenticationFilter Class

---
+++ src/main/java/com/fintech/auth_service/security/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.fintech.auth_service.security.jwt;
+
+/**
+ *
+ * @author Harmony
+ */
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+
+            String token = authHeader.substring(7);
+
+            if (jwtTokenProvider.validateToken(token)) {
+
+                String subject = jwtTokenProvider.getSubject(token);
+                List<String> roles = jwtTokenProvider.getRoles(token);
+
+                var authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                subject,
+                                null,
+                                authorities
+                        );
+
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
